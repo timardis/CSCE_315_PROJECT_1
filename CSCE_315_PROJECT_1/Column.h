1eb2c97e289539a4f2c321e429996e28a36cb7b3
Tim Mardis <timardis12@gmail.com>
2014-02-06 22:36:02 -0600

    Fresh Project

---
+++ CSCE_315_PROJECT_1/Column.h
@@ -0,0 +1,19 @@
+#include <cstdarg>
+#include <string>
+#include <vector>
+
+class Column{
+private:
+	string column_name;
+	string column_type;
+	vector<string> column_data;
+
+public:
+	Column(const string& name, const string& type){
+		column_name = name;
+		column_type = type;
+	}
+	void add_column_data(string data);
+	vector<string> get_column_data();
+	void delete_data(string data);
+};
---
+++ CSCE_315_PROJECT_1/Table.h
@@ -0,0 +1,27 @@
+#include <cstdarg>
+#include <string>
+#include <vector>
+#include "Column.h"
+
+using namespace std;
+
+class Table{
+private:
+
+	string table_name;
+	vector<Column> table_columns;
+
+public:
+	Table(const string& name, const vector<Column>& columns){
+		table_name = name;
+		table_columns = columns;
+
+	}
+
+	void set_table_name(string name);
+	string get_table_name();
+	void delete_table_column(Column c);
+	void add_to_table_column(Column c);
+	vector<Column> get_table_columns();
+
+};
